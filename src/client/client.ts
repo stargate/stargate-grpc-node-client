2d0010aa7d9d7b2ad0235b8055df7181d36a579f
Kia Farhang <kia.farhang@datastax.com>
2021-09-10 13:43:02 -0500

    chore: implement basic promisified example

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,11 +1,22 @@
 import * as grpc from '@grpc/grpc-js';
+import * as util from "util";
 
 import { helloworld } from "../proto/greeter";
 
 const client = new helloworld.GreeterClient('localhost:50051', grpc.credentials.createInsecure());
 
-export const sendMessage = () => {
+export const sendMessage = async () => {
     const message = new helloworld.HelloRequest({name: 'foo'});
+
+    const promisified = util.promisify(client.SayHello).bind(client);
+
+    try {
+        const response = await promisified(message);
+        console.dir(response?.message);
+    } catch (e) {
+        const error = e as grpc.ServiceError;
+        console.dir(error);
+    }
 
     client.SayHello(message, (error, data) => {
         if (error) {
