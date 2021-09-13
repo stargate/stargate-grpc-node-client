c90bc43662b65b5943427a5f79ea236bbaff3ae7
Kia Farhang <kia.farhang@datastax.com>
2021-09-13 11:37:41 -0500

    chore: remove foo folder

--- src/foo/foo.test.ts
+++
@@ -1,7 +0,0 @@
-import {add} from "./foo";
-
-describe("add", () => {
-    it("adds two numbers", () => {
-        expect(add(2,2)).toBe(4);
-    })
-})
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export {add} from "./foo/foo";
-
 import { sendMessage, sendQuery } from "./client/client";
 
 sendQuery();
