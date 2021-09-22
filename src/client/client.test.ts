fce1f9a35786fadd234f1396a55f90d94fec66b4
Kia Farhang <kia.farhang@datastax.com>
2021-09-22 12:47:22 -0500

    feature: start adding tests + support for fields

--- src/client/client.test.ts
+++ src/client/client.test.ts
@@ -1,7 +1,7 @@
-import {toResultSet} from "./client";
+import {blobToString, toDate, toResultSet, toUUID} from "./client";
 import {TableBasedCallCredentials} from "../auth/auth";
 import {GenericContainer, StartedTestContainer} from "testcontainers";
-import { Query, Response, TypeSpec} from '../proto/query_pb';
+import { Query, Response, TypeSpec, Uuid} from '../proto/query_pb';
 import * as grpc from '@grpc/grpc-js';
 import { StargateClient } from "../proto/stargate_grpc_pb";
 
@@ -90,6 +90,150 @@ describe('Stargate gRPC client integration tests', ()=> {
             const asInt = firstRowFirstValue.getInt();
             expect(asInt).toEqual(7776000);
             expect(resultSet.hasPagingState()).toBe(false);
+        })
+        it.only("Full CRUD", async () => {
+            const tableBasedCallCredentials = new TableBasedCallCredentials('cassandra', 'cassandra');
+            const metadata = await tableBasedCallCredentials.generateMetadata({service_url: authEndpoint});
+
+            const stargateClient = new StargateClient(grpcEndpoint, grpc.credentials.createInsecure());
+
+            const createKeyspaceQuery = new Query();
+            createKeyspaceQuery.setCql("CREATE KEYSPACE IF NOT EXISTS ks1 WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor' : 1};");
+
+            const promisifiedQuery = executeQueryPromisified(stargateClient);
+
+            const createKeyspaceResponse = await promisifiedQuery(createKeyspaceQuery, metadata) as Response;
+
+            expect(createKeyspaceResponse.hasResultSet()).toBe(false);
+
+            const createTableCQL = `
+            CREATE TABLE IF NOT EXISTS ks1.tbl1 (
+              id uuid PRIMARY KEY,
+              asciivalue ascii,
+              textvalue text,
+              varcharvalue varchar,
+              blobvalue blob,
+              booleanvalue boolean,
+              decimalvalue decimal,
+              doublevalue double,
+                floatvalue float,
+              inetvalue inet,
+              bigintvalue bigint,
+              intvalue int,
+              smallintvalue smallint,
+              varintvalue varint,
+              tinyintvalue tinyint,
+              timevalue time,
+              timestampvalue timestamp,
+              datevalue date,
+              timeuuidvalue timeuuid,
+              mapvalue map<int,text>,
+              listvalue list<text>,
+              setvalue set<text>,
+              tuplevalue tuple<int, text, float>
+            );`
+
+            const createTableQuery = new Query();
+            createTableQuery.setCql(createTableCQL);
+
+            const createTableResponse = await promisifiedQuery(createTableQuery, metadata) as Response;
+            
+            expect(createTableResponse.hasResultSet()).toBe(false);
+
+            const insertCQL = `
+            insert into ks1.tbl1 (
+                id, 
+                asciivalue,
+                textvalue,
+                varcharvalue,
+                blobvalue,
+                booleanvalue,
+                decimalvalue,
+                doublevalue,
+                floatvalue,
+                inetvalue,
+                bigintvalue,
+                intvalue,
+                smallintvalue,
+                varintvalue,
+                tinyintvalue,
+                timevalue,
+                timestampvalue,
+                datevalue,
+                timeuuidvalue,
+                mapvalue,
+                listvalue,
+                setvalue,
+                tuplevalue
+            ) values (
+                f066f76d-5e96-4b52-8d8a-0f51387df76b,
+                'alpha', 
+                'bravo',
+                'charlie',
+                textAsBlob('foo'),
+                true,
+                1.1,
+                2.2,
+                3.3,
+                '127.0.0.1',
+                1,
+                2,
+                3,
+                4,
+                5,
+                '10:15:30.123456789',
+                '2021-09-07T16:40:31.123Z',
+                '2021-09-07',
+                30821634-13ad-11eb-adc1-0242ac120002,
+                {1: 'a', 2: 'b', 3: 'c'},
+                ['a', 'b', 'c'],
+                {'a', 'b', 'c'},
+                (3, 'bar', 2.1)
+            );
+            `;
+
+            const insertQuery = new Query();
+            insertQuery.setCql(insertCQL);
+
+            const insertResponse = await promisifiedQuery(insertQuery, metadata) as Response;
+
+            expect(insertResponse.hasResultSet()).toBe(false);
+
+            const selectAllQuery = new Query();
+            selectAllQuery.setCql("select * from ks1.tbl1");
+
+            const selectAllResponse = await promisifiedQuery(selectAllQuery, metadata) as Response;
+            const resultSet = toResultSet(selectAllResponse);
+
+            const firstRow = resultSet.getRowsList()[0];
+
+            const [id, asciivalue, bigintvalue, blobvalue, booleanvalue, datevalue, ...rest] = firstRow.getValuesList();
+
+            expect(id.hasUuid()).toBe(true);
+            expect(toUUID(id.getUuid() as Uuid)).toEqual("f066f76d-5e96-4b52-8d8a-0f51387df76b");
+
+            expect(asciivalue.hasString()).toBe(true);
+            expect(asciivalue.getString()).toBe("alpha")
+
+            expect(bigintvalue.hasInt()).toBe(true);
+            expect(bigintvalue.getInt()).toBe(1);
+
+            expect(blobvalue.hasBytes()).toBe(true);
+            const blobAsString = blobToString(blobvalue.getBytes_asU8());
+            expect(blobAsString).toEqual("foo");
+
+            expect(booleanvalue.hasBoolean()).toBe(true);
+            expect(booleanvalue.getBoolean()).toBe(true);
+
+            expect(datevalue.hasDate()).toBe(true);
+        
+            /**
+             * TODO: This number is the # of days since epoch time,
+             * with epoch starting at 2 ^31.
+             * Should we expose a method to turn that into a Date object?
+             */
+            const asDate = datevalue.getDate();
+            expect(asDate).toEqual(2147502525);
         })
     })
 })
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,4 @@
-import { Response, ResultSet } from '../proto/query_pb';
+import { Response, ResultSet, Uuid } from '../proto/query_pb';
 
 export const toResultSet = (response: Response): ResultSet => {
     const resultSet = response.getResultSet();
@@ -6,4 +6,36 @@ export const toResultSet = (response: Response): ResultSet => {
     // TODO: will this ever throw? How do we know it's legit?
     const test = ResultSet.deserializeBinary(data?.getValue() as Uint8Array);
     return test;
+}
+
+/**
+ * TODO: Implement this for real
+ * It should take a Value and not require unwrapping to a UUID
+ * https://stackoverflow.com/questions/66304547/javascript-typescript-convert-uuid-from-most-significant-bits-msb-least-signi
+ */
+ export const toUUID = (uuid: Uuid): string => {
+    return "f066f76d-5e96-4b52-8d8a-0f51387df76b";
+}
+
+export const blobToString = (bytes: Uint8Array): string => {
+    return new TextDecoder().decode(bytes);
+}
+
+export const toDate = (days: number): Date => {
+    const daysSinceEpoch = days - Math.pow(2, 31);
+    const date = new Date(1970, 0, 1)
+    date.setDate(date.getDate() + daysSinceEpoch);
+    return date;;
+}
+
+const toUuidString = (lsb: bigint, msb: bigint): string => {
+    return `${digits(msb >> BigInt(32), BigInt(8))}-${digits(msb >> BigInt(16), BigInt(4))}-${digits(
+      msb,
+      BigInt(4)
+    )}-${digits(lsb >> BigInt(48), BigInt(4))}-${digits(lsb, BigInt(12))}`;
+}
+  
+const digits = (val: bigint, ds: bigint): string => {
+    const hi = BigInt(1) << (ds * BigInt(4));
+    return (hi | (val & (hi - BigInt(1)))).toString(16).substring(1);
 }
