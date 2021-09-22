1450cdcc371c474a280b7fdc3326c5db0ae41868
Kia Farhang <kia.farhang@datastax.com>
2021-09-22 11:19:59 -0500

    test: add an all-numeric integration test

--- src/client/client.test.ts
+++ src/client/client.test.ts
@@ -1,7 +1,7 @@
 import {toResultSet} from "./client";
 import {TableBasedCallCredentials} from "../auth/auth";
 import {GenericContainer, StartedTestContainer} from "testcontainers";
-import { Query, Response} from '../proto/query_pb';
+import { Query, Response, TypeSpec} from '../proto/query_pb';
 import * as grpc from '@grpc/grpc-js';
 import { StargateClient } from "../proto/stargate_grpc_pb";
 
@@ -56,6 +56,40 @@ describe('Stargate gRPC client integration tests', ()=> {
     
             const firstValue = firstRowValues[0].getString();
             expect(firstValue).toEqual("local");
+
+        });
+        it("all numeric", async () => {
+            const tableBasedCallCredentials = new TableBasedCallCredentials('cassandra', 'cassandra');
+            const metadata = await tableBasedCallCredentials.generateMetadata({service_url: authEndpoint});
+
+            const stargateClient = new StargateClient(grpcEndpoint, grpc.credentials.createInsecure());
+
+            const query = new Query();
+            query.setCql("select gc_grace_seconds, default_time_to_live, max_index_interval, memtable_flush_period_in_ms, min_index_interval, read_repair_chance, crc_check_chance, dclocal_read_repair_chance, bloom_filter_fp_chance from system_schema.tables");
+
+            const promisifiedQuery = executeQueryPromisified(stargateClient);
+
+            const response = await promisifiedQuery(query, metadata) as Response;
+
+            const resultSet = toResultSet(response);
+            expect(resultSet.getColumnsList().length).toEqual(9);
+
+            // Check the first column returned
+            const firstColumnSpec = resultSet.getColumnsList()[0];
+            const expectedTypeSpec = new TypeSpec();
+            expectedTypeSpec.setBasic(9);
+            expect(firstColumnSpec.getType()).toEqual(expectedTypeSpec);
+            expect(firstColumnSpec.getName()).toEqual("gc_grace_seconds");
+
+            const rowsReturned = resultSet.getRowsList();
+            expect(rowsReturned.length).toBeGreaterThanOrEqual(37);
+
+            expect(rowsReturned[0].getValuesList().length).toEqual(9);
+
+            const firstRowFirstValue = rowsReturned[0].getValuesList()[0];
+            const asInt = firstRowFirstValue.getInt();
+            expect(asInt).toEqual(7776000);
+            expect(resultSet.hasPagingState()).toBe(false);
         })
     })
 })
