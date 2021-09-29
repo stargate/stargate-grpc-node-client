f7a512fbf2828195f2a06f44331215ccfa83d2cb
Kia Farhang <kia.farhang@datastax.com>
2021-09-29 10:05:19 -0500

    chore: begin exporting elements from main index file

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export {TableBasedCallCredentials} from "./auth/auth";
+export { StargateClient } from "./proto/stargate_grpc_pb";
+export { Collection, Decimal, Payload, Query, QueryParameters, Response, TypeSpec, Uuid} from './proto/query_pb';
